64eda649faf8a486d6c3bc7deb1b67779992e49d
lwc <wence.lv@cnlaunch.com>
2018-10-25 18:07:19 +0800

    构建

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import myLogin from '@/components/login/login'
+import myForgetPassword from '@/components/forgetPassword/forgetPassword'
+import myRegister from '@/components/register/register'
+import myContract from '@/components/register/contract'
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: "/",
+      redirect: "/login"
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: myLogin
+    },
+    {
+      path: '/forgetPassword',
+      name: 'forgetPassword',
+      component: myForgetPassword
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: myRegister
+    },
+    {
+      path: '/contract',
+      name: 'contract',
+      component: myContract
+    },
+  ]
+})
